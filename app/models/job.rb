4f0addc56bd9ec85fb2ac535a7f20d19c247ba5e
alansari <sk.alansari@yahoo.com>
2016-04-18 23:45:24 +0300

    made some changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ActiveRecord::Base
   belongs_to :type
   belongs_to :user
   
-  validates :name, :overview, :type_id, :user_id, presence: true
+  validates :name, :overview, :type_id, presence: true
   
   scope :open, -> {where('filled = ?', "false")}
 end
